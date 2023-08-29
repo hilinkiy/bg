0251f8b54322fe249092756b165d4f915d174819
hilinkiy <hilinkiy@gmail.com>
2023-08-29 15:40:36 +0500

    Add all files

---
+++ main.js
@@ -0,0 +1,20 @@
+const item = document.querySelectorAll('.item');
+const body = document.querySelector('.body');
+
+body.style = 'background: white;';
+
+console.log(item);
+
+for (let i = 0; i < item.length; i++) {
+    console.log(item[i]);
+    item.innerHTML = i + 1;
+
+    console.log(item[i].getAttribute('data-bg'));
+    item[i].style.background = item[i].getAttribute('data-bg')
+
+    item[i].addEventListener("click", function () {
+        if (item[i].style.background == item[i].getAttribute('data-bg')) {
+            body.style.background = item[i].getAttribute('data-bg');
+        }
+    })
+}
